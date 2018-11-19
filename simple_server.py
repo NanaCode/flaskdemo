b71b09ab8f4d6d93d1845b033d9c2390b0a814be
NanaCode <2503605326@qq.com>
2018-11-19 10:37:43 +0800

    debug=True and another approach to add url rule which is app.add_url_rule() func

--- simple_server.py
+++ simple_server.py
@@ -7,9 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello/')  # hello后面加斜杆可以兼容用户输入斜杆的习惯
+# @app.route('/hello/')  # hello后面加斜杆可以兼容用户输入斜杆的习惯
 def hello():
     return 'Hello, Nana!!!'
 
 
-app.run()
+app.add_url_rule('/hello', view_func=hello)  # 另一个注册路由方式
+app.run(debug=True)  # 开启调试模式
