affa270f214af0c04e0a54002c04c40e5d50b31d
NanaCode <2503605326@qq.com>
2018-11-19 14:24:14 +0800

    parameters of app.run() and config file of Flask

---
+++ config.py
@@ -0,0 +1,5 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'Nana'
+__date__ = '2018/11/19 11:46'
+
+Debug = True
--- simple_server.py
+++ simple_server.py
@@ -4,8 +4,12 @@ __date__ = '2018/11/18 17:53'
 
 from flask import Flask
 
-app = Flask(__name__)
+# from config import DEBUG
 
+app = Flask(__name__)
+app.config.from_object('config')  # 注意，传入的参数是模块路径，而不是文件路径
+print(app.config['DEBUG'])
+# print(app.config['Debug'])  # KeyError: 'Debug'
 
 # @app.route('/hello/')  # hello后面加斜杆可以兼容用户输入斜杆的习惯
 def hello():
@@ -13,4 +17,8 @@ def hello():
 
 
 app.add_url_rule('/hello', view_func=hello)  # 另一个注册路由方式
-app.run(debug=True)  # 开启调试模式
+# app.run(debug=True)  # 开启调试模式
+app.run(host='0.0.0.0', debug=True)  # 允许外网访问
+# app.run(host='0.0.0.0', port=80, debug=True)  # 修改端口
+# app.run(host='0.0.0.0', debug=DEBUG)  # 配置文件方式设置是否开启调试模式
+app.run(host='0.0.0.0', debug=app.config['DEBUG'])  # app.config.from_object()方法访问配置文件
