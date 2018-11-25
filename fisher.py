a854f204c98bdab6f738448f528fd390ac03c979
NanaCode <2503605326@qq.com>
2018-11-25 23:07:19 +0800

    1. rename http module to httper module; 2. return json in view func; 3. add classmethod in YuShuBook class.

--- fisher.py
+++ fisher.py
@@ -1,7 +1,10 @@
 # _*_ coding: utf-8 _*_
+from yushu_book import YuShuBook
+
 __author__ = 'Nana'
 __date__ = '2018/11/20 11:43'
 
+import json
 from flask import Flask, make_response
 from helper import is_key_or_isbn
 
@@ -17,6 +20,12 @@ def search(q, page):
     :return:
     """
     key_or_isbn = is_key_or_isbn(q)
+    if key_or_isbn == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+    # return json.dumps(result)  # 纯文本
+    return json.dumps(result), 200, {"content-type": "application/json"}  # 格式化
 
 
 if __name__ == '__main__':
--- yushu_book.py
+++ yushu_book.py
@@ -1,19 +1,21 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'Nana'
 __date__ = '2018/11/22 23:12'
-from http import HTTP
+from httper import HTTP
 
 
 class YuShuBook:
     isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
-    def search_by_isbn(self, isbn):
-        url = YuShuBook.isbn_url.format(isbn)
+    @classmethod
+    def search_by_isbn(cls, isbn):
+        url = cls.isbn_url.format(isbn)
         result = HTTP.get(url)
         return result
 
-    def search_by_keyword(self, keyword):
-        url = YuShuBook.isbn_url.format(keyword)
+    @classmethod
+    def search_by_keyword(cls, keyword, count=15, start=0):
+        url = cls.isbn_url.format(keyword, count, start)
         result = HTTP.get(url)
         return result
