c34f5f7c40170d1a303ea7100099a2f8a81ec4c3
NanaCode <2503605326@qq.com>
2018-12-04 22:32:47 +0800

    use request of flask

--- app/web/book.py
+++ app/web/book.py
@@ -2,19 +2,21 @@
 __author__ = 'Nana'
 __date__ = '2018/11/26 7:25'
 
-from flask import jsonify
+from flask import jsonify, request
 from helper import is_key_or_isbn
 from yushu_book import YuShuBook
 from . import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
     """
     :param q: 普通关键字 isbn
     :param page: start count 缩减而来
     :return:
     """
+    q = request.args['q']
+    page = request.args['page']
     key_or_isbn = is_key_or_isbn(q)
     if key_or_isbn == 'isbn':
         result = YuShuBook.search_by_isbn(q)
