61f1e6c228b67710d3864843fed3d5e483e65dd5
NanaCode <2503605326@qq.com>
2018-12-07 22:50:06 +0800

    add DataRequired() to avoid situation of blank space in url, such as http://localhost:5000/book/search?q= &page=1

--- app/forms/book.py
+++ app/forms/book.py
@@ -3,9 +3,9 @@ __author__ = 'Nana'
 __date__ = '2018/12/4 22:40'
 
 from wtforms import Form, StringField, IntegerField
-from wtforms.validators import Length, NumberRange
+from wtforms.validators import Length, NumberRange, DataRequired
 
 
 class SearchForm(Form):
-    q = StringField(validators=[Length(min=1, max=30)])  # 数组，可以传入多个验证对象
+    q = StringField(validators=[DataRequired(), Length(min=1, max=30)])  # 数组，可以传入多个验证对象
     page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
